5c45083f3811484e34985a5091778ff33556d067
Hrvoje Vukovic <hvukovi@gmail.com>
2014-09-29 23:06:44 +0200

    Validacija za dodaj i izmjeni test

---
+++ TrainingPlanner/Models/TestMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingPlanner.Models
+{
+    public class TestMetadata
+    {
+        [Required(ErrorMessage = "Unesite datum")]
+        [DataType(DataType.Date, ErrorMessage = "Unesite ispravan datum")]
+        public System.DateTime DatumTesta { get; set; }
+
+        [RegularExpression(@"^\s*-?[0-9]{1,5}\s*$", ErrorMessage = "Unesite ispravan broj")]
+        public short Ergometar { get; set; }
+
+        [RegularExpression(@"^\s*-?[0-9]{1,5}\s*$", ErrorMessage = "Unesite ispravan broj")]
+        public short Zgibovi { get; set; }
+
+        [RegularExpression(@"^\s*-?[0-9]{1,5}\s*$", ErrorMessage = "Unesite ispravan broj")]
+        public short Sklekovi { get; set; }
+
+        [RegularExpression(@"^\s*-?[0-9]{1,5}\s*$", ErrorMessage = "Unesite ispravan broj")]
+        public short Trbusnjaci { get; set; }
+
+        [RegularExpression(@"^\s*-?[0-9]{1,5}\s*$", ErrorMessage = "Unesite ispravan broj")]
+        public short Cucnjevi { get; set; }
+
+        [Required(ErrorMessage = "Unesite ime")]
+        [StringLength(15, ErrorMessage = "Maksimum 15 znakova")]
+        public string Name { get; set; }
+    }
+}
